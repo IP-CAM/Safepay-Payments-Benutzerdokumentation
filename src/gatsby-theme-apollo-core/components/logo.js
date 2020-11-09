3c7e7476c46e77d0d83fe5c379fe48f0da066e04
Thomas Smith <me@spud93.com>
2020-11-09 10:42:15 +0100

    Switched to SVGs.
    Signed-off-by: Thomas Smith <me@spud93.com>

--- src/gatsby-theme-apollo-core/components/logo.js
+++ src/gatsby-theme-apollo-core/components/logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/Safepay-logo-01.svg';
 import styled from '@emotion/styled';
 
 const Wrapper = styled.div({
